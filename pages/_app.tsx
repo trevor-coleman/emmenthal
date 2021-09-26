65a32ac0559c1a85f24b4d4e357a294c777b9fed
Trevor Coleman <trevor@trevorcoleman.design>
2021-09-26 17:35:01 -0400

    init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import Head from 'next/head';
+import {AppProps} from 'next/app';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {CacheProvider, EmotionCache} from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/create-emotion-cache';
+import {LocalizationProvider} from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
-export default MyApp
+
+export default function MyApp(props: MyAppProps) {
+  const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Emmenthal</title>
+          <meta name='viewport' content='initial-scale=1, width=device-width'/>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </LocalizationProvider>
+  );
+}
